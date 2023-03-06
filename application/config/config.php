90e244559ce79bd167c51510368e6b672abb1ea1
Ümit Atalan <umit.atalan@gmail.com>
2023-03-06 13:46:35 +0300

    title changed

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/codeigniter-crud-with-ajax/';
+$config['base_url'] = 'http://localhost:88/project/';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Çiloğlu Proje</title>
+    <title>Stok Takip</title>
     <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/datatables/css/dataTables.bootstrap.css')?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/menu.css?v=1')?>" rel="stylesheet">
